069cc4f2ca7ca471c99f682b0cdcc68be0c0cd27
Jack <z_chiz@live.com>
2016-01-17 00:11:26 -0800

    Touch Gestures in progress

--- TouchGestures/app/src/main/java/edu/ucsb/cs/cs190i/evgeny/touchgestures/MainActivity.java
+++ TouchGestures/app/src/main/java/edu/ucsb/cs/cs190i/evgeny/touchgestures/MainActivity.java
@@ -1,13 +1,78 @@
 package edu.ucsb.cs.cs190i.evgeny.touchgestures;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SELECT_IMAGE = 1;
+
+    public static final String ucsbmap = "ucsbmap.png";
+    public static final String ucsbmapSmall = "ucsbmap_small.png";
+    public static final String ucsbmapAerial = "ucsbmapaerial.png";
+    public static final File downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Show ucsbmap.png by default
+        File imageFile = new File(downloadsPath, ucsbmap);
+        Bitmap image = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+        this.setImage(image);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.select_image) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Image"), SELECT_IMAGE);
+        }
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                InputStream imageStream = null;
+                try {
+                    imageStream = getContentResolver().openInputStream(selectedImageUri);
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(this, "Could not open image file!",  Toast.LENGTH_SHORT).show();
+                }
+                Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                this.setImage(selectedImage);
+            }
+        }
+    }
+
+    private void setImage(Bitmap image) {
+        TouchImageView touchImageVIew = (TouchImageView) findViewById(R.id.touch_image_view);
+        touchImageVIew.setImageBitmap(image);
     }
 }
---
+++ TouchGestures/app/src/main/java/edu/ucsb/cs/cs190i/evgeny/touchgestures/TouchImageView.java
@@ -0,0 +1,205 @@
+package edu.ucsb.cs.cs190i.evgeny.touchgestures;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+public class TouchImageView extends ImageView {
+
+    private Matrix matrix;
+    private ScaleGestureDetector scaleGestureDetector;
+    private MyDragGestureDetector dragGestureDetector;
+    private MyRotateGestureDetector rotateGestureDetector;
+
+    public TouchImageView(Context context) {
+        super(context);
+        this.initialize();
+    }
+
+    public TouchImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.initialize();
+    }
+
+    public TouchImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.initialize();
+    }
+
+    public TouchImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.initialize();
+    }
+
+    public void initialize() {
+        this.scaleGestureDetector = new ScaleGestureDetector(this.getContext(), new MyScaleGestureListener());
+        this.dragGestureDetector = new MyDragGestureDetector();
+        this.rotateGestureDetector = new MyRotateGestureDetector();
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap mp) {
+        super.setImageBitmap(mp);
+        this.matrix = this.getImageMatrix();
+    }
+
+    @Override
+    protected void onDraw (Canvas canvas) {
+        // Apply matrix before drawing
+        this.setImageMatrix(matrix);
+        // Draw
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Scale gesture handling
+        this.scaleGestureDetector.onTouchEvent(event);
+
+        // Drag gesture handling
+        this.dragGestureDetector.onTouchEvent(event);
+
+        // Rotation gesture handling
+        this.rotateGestureDetector.onTouchEvent(event);
+
+        return true;
+    }
+
+    ///////////////////
+    // Scale Gesture //
+    ///////////////////
+    private class MyScaleGestureListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        private PointF scaleFocalPoint = new PointF(0, 0);
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            this.scaleFocalPoint.set(detector.getFocusX(), detector.getFocusY());
+            return true;
+        }
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float scaleFactor = detector.getScaleFactor();
+            matrix.postScale(scaleFactor, scaleFactor, scaleFocalPoint.x, scaleFocalPoint.y);
+            invalidate();
+            return true;
+        }
+    }
+
+    //////////////////
+    // Drag Gesture //
+    //////////////////
+    private class MyDragGestureDetector {
+        private PointF activePointerLocation = new PointF(0, 0);
+        private int activePointerId = -1; // -1 for "undefined"
+
+        public boolean onTouchEvent(MotionEvent event) {
+            int action = event.getActionMasked();
+            int currentActionIndex = event.getActionIndex();
+            int currentPointerId = event.getPointerId(currentActionIndex);
+            int activePointerIndex = event.findPointerIndex(activePointerId);
+
+            // Primary pointer going down
+            if (action == MotionEvent.ACTION_DOWN) {
+                activePointerId = currentPointerId;
+                activePointerLocation.set(event.getX(), event.getY());
+            }
+            // Primary pointer is being released
+            else if (action == MotionEvent.ACTION_UP) {
+                activePointerId = -1;
+            }
+            // Primary pointer is being released but more pointers remain
+            else if (activePointerId == currentPointerId && action == MotionEvent.ACTION_POINTER_UP) {
+                int pointerCount = event.getPointerCount();
+                int pointerIndex = (activePointerIndex + 1) % pointerCount;
+                activePointerId = event.getPointerId(pointerIndex);
+                activePointerLocation.set(event.getX(pointerIndex), event.getY(pointerIndex));
+            }
+            // Primary pointer moved
+            else if (activePointerId != -1 && action == MotionEvent.ACTION_MOVE) {
+                float curX = event.getX(activePointerIndex);
+                float curY = event.getY(activePointerIndex);
+                matrix.postTranslate(curX - activePointerLocation.x, curY - activePointerLocation.y);
+                activePointerLocation.set(curX, curY);
+                invalidate();
+            }
+
+            return true;
+        }
+    }
+
+    ////////////////////
+    // Rotate Gesture //
+    ////////////////////
+    private class MyRotateGestureDetector {
+        private PointF firstPointerLocation = new PointF(0, 0);
+        private PointF secondPointerLocation = new PointF(0, 0);
+        private PointF pivotLocation = new PointF(0, 0);
+        private int firstPointerId = -1; // -1 for "undefined"
+        private int secondPointerId = -1; // -1 for "undefined"
+
+        public boolean onTouchEvent(MotionEvent event) {
+            int action = event.getActionMasked();
+            int currentActionIndex = event.getActionIndex();
+            int currentPointerId = event.getPointerId(currentActionIndex);
+            int pointerCount = event.getPointerCount();
+            int firstPointerIndex = event.findPointerIndex(firstPointerId);
+            int secondPointerIndex = event.findPointerIndex(secondPointerId);
+
+            // First pointer going down
+            if (action == MotionEvent.ACTION_DOWN) {
+                firstPointerId = currentPointerId;
+                firstPointerLocation.set(event.getX(), event.getY());
+            }
+            // Second pointer going down
+            else if (action == MotionEvent.ACTION_POINTER_DOWN && pointerCount == 2) {
+                secondPointerId = currentPointerId;
+                secondPointerIndex = event.findPointerIndex(secondPointerId);
+                secondPointerLocation.set(event.getX(secondPointerIndex), event.getY(secondPointerIndex));
+                pivotLocation = getMiddlePoint(firstPointerLocation, secondPointerLocation);
+            }
+            // Two tracked pointers are down, moving
+            else if (pointerCount == 2 && firstPointerId != -1 && secondPointerId != -1 && action == MotionEvent.ACTION_MOVE) {
+                PointF newFirstPointerLocation = new PointF(event.getX(firstPointerIndex), event.getY(firstPointerIndex));
+                PointF newSecondPointerLocation = new PointF(event.getX(secondPointerIndex), event.getY(secondPointerIndex));
+                double rotationAngle = getAngle(firstPointerLocation, secondPointerLocation, newFirstPointerLocation, newSecondPointerLocation);
+
+                matrix.postRotate((float)rotationAngle, pivotLocation.x, pivotLocation.y);
+                firstPointerLocation = newFirstPointerLocation;
+                secondPointerLocation = newSecondPointerLocation;
+                invalidate();
+            }
+            else if (action == MotionEvent.ACTION_POINTER_UP) {
+                if (firstPointerId == currentPointerId) {
+                    firstPointerId = -1;
+                } else if (secondPointerId == currentPointerId) {
+                    secondPointerId = -1;
+                }
+            }
+            else if (action == MotionEvent.ACTION_UP) {
+                firstPointerId = secondPointerId = -1;
+            }
+
+            return true;
+        }
+
+        private double getAngle(PointF p1, PointF p2, PointF np1, PointF np2) {
+            double oldAtan = Math.atan2(p1.y - p2.y, p1.x - p2.x);
+            double newAtan = Math.atan2(np1.y - np2.y, np1.x - np2.x);
+            double angle = Math.toDegrees(newAtan - oldAtan) % 360;
+            if (angle < -180) angle += 360;
+            if (angle > 180) angle -= 360;
+            return angle;
+        }
+
+        private PointF getMiddlePoint(PointF p1, PointF p2) {
+            return new PointF((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+        }
+    }
+}
