7f1c8e9219569916c7a0214ee97aeab9417fbdaa
Jack <z_chiz@live.com>
2016-02-12 16:08:22 -0800

    Animation fixed

--- EvgenyDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/evgeny/evgenydemosuite/FragmentAnimation.java
+++ EvgenyDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/evgeny/evgenydemosuite/FragmentAnimation.java
@@ -10,27 +10,26 @@ import android.view.ViewGroup;
 public class FragmentAnimation extends Fragment {
 
     private DrawingThread thread = null;
-    private int fps = 50;
+    private static final int FPS = 50;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animation, container, false);
+        View layout = inflater.inflate(R.layout.fragment_animation, container, false);
+        AnimationView animationView = (AnimationView) layout.findViewById(R.id.animationView);
+        this.thread = new DrawingThread(animationView, FPS);
+        return layout;
     }
 
     private void playAnimation() {
-        AnimationView animationView = (AnimationView) getView().findViewById(R.id.animationView);
-        if (this.thread == null) {
-            this.thread = new DrawingThread(animationView, fps);
-        }
         if (!this.thread.isRunning()) {
             this.thread.start();
         }
     }
 
     private void pauseAnimation() {
-        if (this.thread != null && this.thread.isRunning()) {
+        if (this.thread.isRunning()) {
             this.thread.stop();
         }
     }
